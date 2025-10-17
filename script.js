55f87b9e71ca0c5762d94119651775dc41f2a6c7
udinndinn49-debug <udinndinn49@gmail.com>
2025-10-17 10:53:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // script.js
 
 // ===== Inisialisasi Supabase Client (PERBAIKAN) =====
-const SUPABASE_URL = 'URL_SUPABASE_ANDA'; // Pastikan ini benar
-const SUPABASE_ANON_KEY = 'ANON_KEY_ANDA'; // Pastikan ini benar
+const SUPABASE_URL = 'https://zisateevmyushnucnwfs.supabase.co'; // Pastikan ini benar
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inppc2F0ZWV2bXl1c2hudWNud2ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA1NjI2MzUsImV4cCI6MjA3NjEzODYzNX0.ssUcxnfZ2Fdwj3J2cG50t6ynvnXNIFkmZdnCnxSzjc0'; // Pastikan ini benar
 
 // Panggil createClient dari objek 'supabase' global (dari CDN)
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
